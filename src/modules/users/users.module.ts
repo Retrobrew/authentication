24c8dcd40398ad97890efe016e9a51c041906496
ame <amedouillard@hotmail.fr>
2022-04-25 23:16:18 +0200

    get requests

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -9,10 +9,16 @@ import { FriendRequestController } from './exposition/friendship/friend-request.
 import { FriendRequest } from './domain/entities/friend-request.entity';
 import { AnswerFriendshipRequestService } from './application/services/Friendship/answer-friendship-request.service';
 import { AnswerFriendRequestController } from './exposition/friendship/answer-friend-request.controller';
+import { GetFriendRequestsController } from './exposition/friendship/get-friend-requests.controller';
 
 @Module({
   imports: [ MikroOrmModule.forFeature({ entities: [User, FriendRequest] }) ],
-  controllers: [ UsersController, FriendRequestController, AnswerFriendRequestController ],
+  controllers: [
+    UsersController,
+    FriendRequestController,
+    AnswerFriendRequestController,
+    GetFriendRequestsController
+  ],
   providers: [
     UsersService,
     UserRepository,
