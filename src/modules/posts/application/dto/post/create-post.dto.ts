4423c8a32da7ee11807f5ad6e502c61140d89ce8
ame <amedouillard@hotmail.fr>
2022-05-03 18:49:06 +0200

    déplacement

--- src/modules/posts/application/dto/post/create-post.dto.ts
+++ src/modules/posts/application/dto/post/create-post.dto.ts
@@ -1,4 +1,4 @@
-import { User } from '../../../users/domain/entities/user.entity';
+import { User } from '../../../../users/domain/entities/user.entity';
 
 export class CreatePostDto {
   title: string;
