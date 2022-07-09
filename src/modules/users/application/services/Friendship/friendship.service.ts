1f82b031dcc2b4d2634224129f24f415d5b994c5
ame <amedouillard@hotmail.fr>
2022-07-09 11:29:03 +0200

    fix unfriend

--- src/modules/users/application/services/Friendship/friendship.service.ts
+++ src/modules/users/application/services/Friendship/friendship.service.ts
@@ -3,15 +3,18 @@ import { User } from '../../../domain/entities/user.entity';
 import { UserRepository } from '../../user.repository';
 import { EntityRepository } from '@mikro-orm/mysql';
 import { FriendDto } from '../../dto/user/friend.dto';
-import { BadRequestException } from '@nestjs/common';
+import { BadRequestException, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
 import { Friendship } from '../../../domain/entities/friendship.entity';
+import { FriendRequest } from '../../../domain/entities/friend-request.entity';
 
 export class FriendshipService {
   constructor(
     @InjectRepository(User)
     private readonly userRepository: UserRepository,
     @InjectRepository(Friendship)
-    private readonly friendRepository: EntityRepository<Friendship>
+    private readonly friendRepository: EntityRepository<Friendship>,
+    @InjectRepository(FriendRequest)
+    private readonly friendRequestRepository: EntityRepository<FriendRequest>
   ) {}
 
   async getFriends(userUuid: string): Promise<Array<FriendDto>> {
@@ -40,5 +43,57 @@ export class FriendshipService {
 
       return friends;
     })
+  }
+
+  async unfriend(friendUuid: string, userUuid: string) {
+    const user = await this.getUser(userUuid);
+    const friend = await this.getUser(friendUuid);
+
+    const friendship = await this.friendRepository.findOne({friendA: user, friendB: friend})
+    if(!friendship) {
+      throw new BadRequestException("You can't do that")
+    }
+
+    const reverseFriendship = await this.friendRepository.findOne({friendA: friend, friendB: user})
+    if(!reverseFriendship) {
+      throw new BadRequestException("You can't do that")
+    }
+
+    let friendRequest = await this.friendRequestRepository.findOne({
+      requester: user,
+      recipient: friend,
+      status: "accepted"
+    })
+
+    if(!friendRequest) {
+      friendRequest = await this.friendRequestRepository.findOne({
+        requester: friend,
+        recipient: user,
+        status: "accepted"
+      })
+    }
+
+    if(!friendRequest) {
+      throw new InternalServerErrorException("Could not find friend request")
+    }
+
+    await this.friendRepository.removeAndFlush(friendship);
+    await this.friendRepository.removeAndFlush(reverseFriendship);
+    await this.friendRequestRepository.removeAndFlush(friendRequest);
+  }
+
+  private async getUser(userUuid: string): Promise<User> {
+    let user = await this.userRepository.findOne({uuid: userUuid}, {populate: true})
+
+    if (!user) {
+      Logger.error(
+        `Erreur lors de la récupération de l'utilisateur ${userUuid}`,
+        '',
+        this.constructor.name
+      )
+      throw new NotFoundException(`Utilisateur ${userUuid} inconnu`);
+    }
+
+    return user;
   }
 }
