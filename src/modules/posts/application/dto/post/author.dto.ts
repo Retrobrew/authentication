a3c08daac15a5ed06f8b26b6944201a90b1b9845
ame <amedouillard@hotmail.fr>
2022-07-09 18:56:49 +0200

    author-dto retourne picture

--- src/modules/posts/application/dto/post/author.dto.ts
+++ src/modules/posts/application/dto/post/author.dto.ts
@@ -1,12 +1,15 @@
 export class AuthorDto {
   public readonly uuid: string;
   public readonly username: string;
+  public readonly picture: string;
 
   constructor(
     uuid: string,
-    username: string
+    username: string,
+    picture: string = null
   ) {
     this.uuid = uuid;
     this.username = username;
+    this.picture = picture;
   }
 }
